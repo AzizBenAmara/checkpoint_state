ee3dc33e58adbd005db18072772bb3f25add8337
aziz benamara <azizbenamara@TUN-JWT-1914.local>
2022-04-09 12:05:24 +0100

    teqst

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
+import Home from "./Home";
+import ImgSrc from "./avatar.jpg";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class componentName extends Component {
+  state = { fullname: "Aziz Ben Amara", image: ImgSrc, show: false, count: 0 };
+  
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  handleDecrement = () => {
+    this.state.count > 0 && this.setState({ count: this.state.count - 1 });
+  };
 
-export default App;
+  /*  componentDidMount() {
+    alert(`welcome ${this.state.fullname}`);
+  }
+  componentDidUpdate() {
+    alert("your component and i mean by that the state is updated");
+  } */
+
+  handleShow = () => {
+    if (this.state.show) {
+      this.setState({ show: false });
+    } else {
+      setInterval(() => {
+        this.setState({ count: this.state.count + 1 });
+      }, 1000);
+      this.setState({ count: 0 });
+      this.setState({ show: true });
+    }
+  };
+
+  render() {
+    return (
+      <div className="div1">
+        {/* <button onClick={this.handleIncrement}>increment</button>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleDecrement}>decrement</button> */}
+        {this.state.show && (
+          <div className="div2">
+            <h1>{this.state.fullname}</h1>
+            <img className="image" src={this.state.image} />
+            <h1 className="count">{this.state.count}</h1>
+
+            {/* {this.state.show && <Home />} */}
+          </div>
+        )}
+        <button className="button1" onClick={this.handleShow}>SHOW</button>
+      </div>
+    );
+  }
+}
